1c3d80b3803255f75d32f92ec84ec7ddb5867ea7
AuthorMax <maxim_terekhin@bk.ru>
2022-02-27 22:48:22 +0300

    validation programm

--- Задача_2/Program.cs
+++ Задача_2/Program.cs
@@ -1,3 +1,17 @@
 ﻿Console.Write("Введите первое число: ");
 double numberFirst = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine(numberFirst);
+double numberSecond = Convert.ToDouble(Console.ReadLine());
+double maxNumber;
+double minNumber;
+if (numberFirst < numberSecond)
+{
+    minNumber = numberFirst;
+    maxNumber = numberSecond;
+}
+else
+{
+    minNumber = numberSecond;
+    maxNumber = numberFirst;
+}
+Console.WriteLine("Большее число: " + maxNumber);
+    Console.WriteLine("Наименьшее число: " + minNumber);
