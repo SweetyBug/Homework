9fd07e90544970664ed1ca6fc27394869d2699f7
AuthorMax <maxim_terekhin@bk.ru>
2022-02-27 23:19:05 +0300

    finish changes

--- Задача_6/Program.cs
+++ Задача_6/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите число:");
+double number = Convert.ToDouble(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Да");
+
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
