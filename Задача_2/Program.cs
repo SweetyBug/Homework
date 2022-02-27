fa2d34fa98f8e5a87572ab745d6e28600646c91f
AuthorMax <maxim_terekhin@bk.ru>
2022-02-27 22:55:27 +0300

    Finish changes

--- Задача_2/Program.cs
+++ Задача_2/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.Write("Введите первое число: ");
 double numberFirst = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
 double numberSecond = Convert.ToDouble(Console.ReadLine());
 double maxNumber;
 double minNumber;
@@ -7,11 +8,17 @@ if (numberFirst < numberSecond)
 {
     minNumber = numberFirst;
     maxNumber = numberSecond;
+    Console.WriteLine("Наибольшее число: " + maxNumber);
+    Console.WriteLine("Наименьшее число: " + minNumber);
+}
+else if (numberFirst == numberSecond)
+{
+    Console.WriteLine("Оба числа равны!");
 }
 else
 {
     minNumber = numberSecond;
     maxNumber = numberFirst;
-}
-Console.WriteLine("Большее число: " + maxNumber);
+    Console.WriteLine("Наибольшее число: " + maxNumber);
     Console.WriteLine("Наименьшее число: " + minNumber);
+}
