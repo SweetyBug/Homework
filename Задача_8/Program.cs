4614e3fb21abfce1a5d8f8607bfcd4d8a6c4e48a
AuthorMax <maxim_terekhin@bk.ru>
2022-02-27 23:38:08 +0300

    finish code

--- Задача_8/Program.cs
+++ Задача_8/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i < number + 1) 
+{
+    if (i % 2 == 0) Console.WriteLine(i);
+    i += 1;
+}
