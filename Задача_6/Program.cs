8b5ccf3a46d96eb3d76df25e06926514562a1647
AuthorMax <maxim_terekhin@bk.ru>
2022-02-27 23:29:23 +0300

    full changes

--- Задача_6/Program.cs
+++ Задача_6/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите число:");
-double number = Convert.ToDouble(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 if (number % 2 == 0)
 {
     Console.WriteLine("Да");
